4b4ea4f8557d441a21f016c41b8d2bd4271991f8
servispacement-cpu <servispacement@gmail.com>
2026-03-18 14:06:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function fb() {
 document.getElementById("bl").onclick = fl;
 
 function fl() {
-    window.location.href = "cavlion/index.html";
+    window.location.href = "cavl'angeaoût/index.html";
 }
 
 
