71f807c27b2fdf90b89f78649e25cc2450fddf50
servispacement-cpu <servispacement@gmail.com>
2026-04-01 12:52:53 +0200

    Add files via upload

---
+++ tools.js
@@ -0,0 +1,38 @@
+
+
+async function requete(id, nom, age){
+    const url = 'http://localhost:3000/items';
+    const payload = { id, nom, age };
+
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(payload)
+        });
+
+        if (!response.ok) throw new Error(`Erreur HTTP : ${response.status}`);
+
+        const data = await response.json();
+        console.log('Réponse du serveur :', data);
+    } catch (error) {
+        console.error('Erreur :', error);
+    }
+}
+
+
+async function requeteget(id){
+    const url = 'http://localhost:3000/item?id='+id;
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (!response.ok) throw new Error(`Erreur HTTP : ${response.status}`);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Erreur :', error);
+    } 
+}
